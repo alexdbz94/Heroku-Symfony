28d750d0af3f36d9aea6dcb8fba65a50789c815e
Alexandre Tournier <alexandretournier@MacBook-de-Alexandre-Tournier.local>
2015-02-27 14:25:05 +0100

    iter 1 - try session

--- src/Lyckee/StoreBundle/Controller/DefaultController.php
+++ src/Lyckee/StoreBundle/Controller/DefaultController.php
@@ -8,6 +8,15 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('LyckeeStoreBundle:Default:index.html.twig', array('name' => $name));
+        
+        $session = $request->getSession();
+
+        // store an attribute for reuse during a later user request
+        $session->set('Panier', 'Warhol');
+
+        // get the attribute set by another controller in another request
+        $var = $session->get('Panier');
+        
+        return $this->render('LyckeeStoreBundle:Default:index.html.twig', array('name' => $name, 'Panier'=>$var));
     }
 }
